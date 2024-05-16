65bb0c620dc0fa462253fc0636c901b046c8fde4
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-05-16 09:39:27 -0600

    Add files via upload

---
+++ POO_2024/2_variables_tipos_datos/variables.py
@@ -0,0 +1,25 @@
+#las variables son debilmente tipadas
+#es decir no es necesario indicar el tipo de 
+#dato al momento de utilizarlas
+
+
+#VARIABLES
+edad=30
+estatura=1.80
+email="alfonso@gmail.com"
+logica=True
+caracter='@'
+
+#IMPRIMIR CONTENIDO DE VARIABLES
+print(edad)
+print(estatura)
+print(email)
+print(logica)
+print(caracter)
+
+#CONOCER EL TIPO DE DATO QUE PYTHON ASIGNO
+print(type(edad))
+print(type(estatura))
+print(type(email))
+print(type(logica))
+print(type(caracter))
