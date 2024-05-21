6bbf0b956a64447f3a9c29e62063ec316779640b
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-05-21 10:24:47 -0600

    s

--- POO_2024/parcial1_2A/5_estructuras_control_ciclicas/for.py
+++ POO_2024/parcial1_2A/5_estructuras_control_ciclicas/for.py
@@ -1,12 +1,12 @@
 #Ciclo FOR Estructura iterativa que se ejecuta X veces
 
 #Sintaxis
-#..For variable in elemento_iterable (lista, rango, etc)
+#..For variable in elemento_iterable (lista, rango, etc):
 #..Bloque de instrucciones
 
 #Ejemplo 1 Crear un programa que imprima en pantalla 5 veces el @
 
-contador=1
+
 for contador in range(1,6):
     print("@")
     print()
@@ -14,7 +14,7 @@ for contador in range(1,6):
 
     #Ejemplo 2 crear un programa que imprima los numeros del 1 al 5 y los sume y al final imprima la suma
 
-    contador=1
+  
     suma=0
     for contador in range(1,6):
         print(contador)
@@ -26,7 +26,7 @@ for contador in range(1,6):
         #Ejemplo 3 Crear un programa que imprima la tabla de multiplicar que el usuario desee
         
         tabla=int(input("Ingresa un numero para calcular su tabla de multiplicar: "));
-        i=1
+        
         multi=0
 
         for i in range(1,11):
