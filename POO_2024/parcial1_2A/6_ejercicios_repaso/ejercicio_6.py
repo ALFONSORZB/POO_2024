6917d9e17563af9f7d3a557bb530a8731f8e53de
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-05-21 10:58:48 -0600

    si

--- POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_6.py
+++ POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_6.py
@@ -1,2 +1,9 @@
 #Mostrar todas las tablas del 1 al 10. Mostrando el titulo de la tabla y luego las multiplicaciones del 1 al 10
-
+#Alfonso Ramirez Bravo
+def tablas():
+    for i in range(1, 11):  
+        print(f"Tabla del {i}")
+        for n in range(1, 11):  
+            print(f"{i} x {n} = {i * n}")
+        print()  
+tablas()
