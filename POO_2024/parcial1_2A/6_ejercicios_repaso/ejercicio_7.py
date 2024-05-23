f21e31462d8b191a661407da18ec77b864e93325
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-05-23 10:31:01 -0600

    si

--- POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_7.py
+++ POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_7.py
@@ -2,3 +2,14 @@
 #Alfonso Ramirez Bravo
 
 
+n1 = int(input("Ingresa el primer numero: "))
+n2 = int(input("Ingresa el segundo numero: "))
+
+if n1 < n2:
+    for i in range(n1, n2):
+        if i % 2 != 0:
+            print(i)
+elif n1 > n2:
+    for i in range(n2, n1):
+        if i % 2 != 0:
+            print(i)
--- POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_8.py
+++ POO_2024/parcial1_2A/6_ejercicios_repaso/ejercicio_8.py
@@ -1,2 +1,9 @@
 # Hacer un programa que resuelva lo siguiente. ¿Cuanto es el X por ciento de X numero?
 
+
+
+numero = int(input("Ingresa un numero: "))
+porcentaje = int(input(f"Que porcentaje del numero {numero} quieres saber?: "))
+
+resultado = (numero * porcentaje) / 100
+print(f"El {porcentaje}% de {numero} es: {resultado}")
