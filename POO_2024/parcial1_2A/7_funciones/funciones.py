d7afbf8246e7ffc75326d904d00842adf07bd940
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-05-30 09:38:46 -0600

    si

--- POO_2024/parcial1_2A/7_funciones/funciones.py
+++ POO_2024/parcial1_2A/7_funciones/funciones.py
@@ -15,4 +15,31 @@
     #2.-Funcion que no recibe parametros y regresa valor
     #3.-Funcion que recibe parametros y no regresa valor
     #4.-Funcion que recibe parametros y regresa valor
-    #Al
+    
+    
+    #1.-Funcion que no recibe parametros y no regresa valor
+
+def sumaNumeros1():
+        n1=int(input("Numero # 1:"))
+        n2=int(input("Numero # 2:"))
+        suma=n1+n2
+        print(f"{n1}+{n2}={suma}")
+
+    #2.-Funcion que no recibe parametros y regresa valor
+def sumaNumeros2():
+    n1=int(input("Numero # 1:"))
+    n2=int(input("Numero # 2:"))
+    suma=n1+n2
+    return (f"{n1}+{n2}={suma}")
+
+cadena=sumaNumeros2()
+print(cadena) 
+
+    #3.-Funcion que recibe parametros y no regresa valor
+def sumaNumeros3(n1,n2):
+    suma=n1+n2
+    print(f"{n1}+{n2}={suma}")
+
+num1=23
+num2=32
+sumaNumeros3(num1,num2) 
