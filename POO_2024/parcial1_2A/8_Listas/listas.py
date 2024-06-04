aeb9371cd21775c5f259d58356d495f8d1501cdb
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-06-04 10:52:49 -0600

    si

--- POO_2024/parcial1_2A/8_Listas/listas.py
+++ POO_2024/parcial1_2A/8_Listas/listas.py
@@ -1,5 +1,26 @@
 # List (Arrays)son colecciones o conjuntos de datos/valores bajo un mismo nombre, para acceder a los valores
- #     se hace como un indice numerico
-  #    
-   #   Nota: Sus valores si son modificables
-    #  La lista es una coleccion ordenada y modificable. Permite miembros duplicados.
+#Se hace como un indice numerico    
+#Nota: Sus valores si son modificable
+#La lista es una coleccion ordenada y modificable. Permite miembros duplicados.
+
+
+
+#Ejemplo 1 Crear una lista con valores numericos enteros e imprimir la lista
+
+numeros=23
+numeros=34
+
+numeros=[23,34]
+print(numeros)
+
+#Recorrer la lista con un FOR
+for i in numeros:
+    print(i)
+    
+    
+#Recorrer la lista con un WHILE
+i=0
+while i<len (numeros):
+    print(numeros[i])
+    i+=1
+    
