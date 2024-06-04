fc11d769477b4c295d5bee7afb56a569a3797dfb
ALFONSORZB <alfonso_3041240001@utd.edu.mx>
2024-06-04 10:51:40 -0600

    si

---
+++ POO_2024/parcial1_2A/8_Listas/listas.py
@@ -0,0 +1,5 @@
+# List (Arrays)son colecciones o conjuntos de datos/valores bajo un mismo nombre, para acceder a los valores
+ #     se hace como un indice numerico
+  #    
+   #   Nota: Sus valores si son modificables
+    #  La lista es una coleccion ordenada y modificable. Permite miembros duplicados.
